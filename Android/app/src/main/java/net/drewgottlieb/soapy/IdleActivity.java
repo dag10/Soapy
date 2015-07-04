9420b049cf6b007ce14121774e47c73069824b08
Drew Gottlieb <gottlieb.drew@gmail.com>
2015-07-04 15:06:50 -0700

    Full-screen "hello world" android app.

---
+++ Android/app/src/main/java/net/drewgottlieb/soapy/IdleActivity.java
@@ -0,0 +1,14 @@
+package net.drewgottlieb.soapy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class IdleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idle);
+    }
+}

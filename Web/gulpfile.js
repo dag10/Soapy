92f98ce51f7e17dab6d5b4024dafcc2594e66676
Drew Gottlieb <gottlieb.drew@gmail.com>
2016-02-03 19:01:15 -0500

    Web: Added basic Angular/TS build system with npm and gulp.
    Adds npm, gulp, typescript, and several dependencies such as
    angular2 and es6 polyfills.
    
    Guided by:
    http://blog.scottlogic.com/2015/12/24/creating-an-angular-2-build.html

---
+++ Web/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var del = require('del');
+var typescript = require('gulp-typescript');
+var tscConfig = require('./tsconfig.json');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsSource = 'typescript/**/';
+var jsOut = 'public/js/';
+var tsOut = jsOut + 'app/';
+var libOut = jsOut + 'lib/';
+
+// Clean the contents of the distribution directory
+gulp.task('clean', function () {
+  return del(jsOut + '**/*');
+});
+
+// TypeScript compile
+gulp.task('compile', ['clean'], function() {
+  return gulp
+    .src(tsSource + '*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(tsOut));
+});
+
+// Copy static assets (non TypeScript compiled files)
+gulp.task('copy:assets', ['clean'], function() {
+  return gulp.src(['app/**/*', '!app/**/*.ts'], { base : './' })
+    .pipe(gulp.dest('dist'))
+});
+
+// Copy dependencies
+gulp.task('copy:libs', ['clean'], function() {
+  return gulp.src([
+    'node_modules/angular2/bundles/angular2-polyfills.js',
+    'node_modules/systemjs/dist/system.src.js',
+    'node_modules/rxjs/bundles/Rx.js',
+    'node_modules/angular2/bundles/angular2.dev.js',
+    'node_modules/angular2/bundles/router.dev.js'
+  ])
+  .pipe(gulp.dest(libOut));
+});
+
+gulp.task('build', ['compile', 'copy:libs']);
+gulp.task('default', ['build']);
+

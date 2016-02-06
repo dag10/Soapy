6dac15b010c6b4d3299eaf51489f560a45464ed7
Drew Gottlieb <gottlieb.drew@gmail.com>
2016-02-06 16:13:03 -0500

    Web: Switched from CSS to LESS.

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -4,22 +4,46 @@ var typescript = require('gulp-typescript');
 var tscConfig = require('./tsconfig.json');
 var sourcemaps = require('gulp-sourcemaps');
 var tslint = require('gulp-tslint');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+
 
 var tsSource = 'typescript/**/';
 var tsOut = 'public/app/';
 var libOut = 'public/lib/';
 
-// Clean the contents of the distribution directory
+var lessSource = 'less/**/';
+var cssOut = 'public/css/';
+
+// Clean the compiles and copied javascript and style resources
 gulp.task('clean', function () {
   return del([tsOut + '**/*',
-              libOut + '**/*']);
+              libOut + '**/*',
+              cssOut + '**/*']);
 });
 
 // TypeScript lint
 gulp.task('tslint', function() {
-  return gulp.src(tsSource + '*.ts')
+  return gulp
+    .src(tsSource + '*.ts')
     .pipe(tslint())
     .pipe(tslint.report('verbose'));
+});
+
+// Compile LESS to CSS
+gulp.task('less', ['clean'], function() {
+  return gulp
+    .src(lessSource + '*.less')
+    .pipe(less())
+    .pipe(gulp.dest(cssOut));
+});
+
+// Copy non-less CSS files
+gulp.task('copy:css', ['clean'], function() {
+  return gulp
+    .src([lessSource + '*', '!' + lessSource + '*.less'],
+         { base : './less' })
+    .pipe(gulp.dest(cssOut));
 });
 
 // TypeScript compile
@@ -34,14 +58,16 @@ gulp.task('compile', ['clean'], function() {
 
 // Copy static assets (non TypeScript compiled files)
 gulp.task('copy:assets', ['clean'], function() {
-  return gulp.src([tsSource + '*', '!' + tsSource + '*.ts'],
-                  { base : './typescript' })
+  return gulp
+    .src([tsSource + '*', '!' + tsSource + '*.ts'],
+         { base : './typescript' })
     .pipe(gulp.dest(tsOut));
 });
 
 // Copy dependencies
 gulp.task('copy:libs', ['clean'], function() {
-  return gulp.src([
+  return gulp
+    .src([
     'node_modules/es6-shim/es6-shim.js',
     'node_modules/es6-shim/es6-shim.min.js',
     'node_modules/es6-shim/es6-shim.map',
@@ -72,6 +98,14 @@ gulp.task('copy:libs', ['clean'], function() {
   .pipe(gulp.dest(libOut));
 });
 
-gulp.task('build', ['tslint', 'compile', 'copy:assets', 'copy:libs']);
+gulp.task('build', [
+    'tslint',
+    'less',
+    'copy:css',
+    'compile',
+    'copy:assets',
+    'copy:libs'
+  ]);
+
 gulp.task('default', ['build']);
 

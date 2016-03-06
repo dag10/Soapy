539882ca89acaed55628cd66023eefc05b5ca5ec
Drew Gottlieb <gottlieb.drew@gmail.com>
2016-03-06 02:15:52 -0500

    Web: Removed playlist dropdown component.

--- Web/typescript/playback.component.ts
+++ Web/typescript/playback.component.ts
@@ -1,31 +1,31 @@
 import {Component, OnInit, ChangeDetectorRef} from 'angular2/core';
 
 import {Playlist} from './soapy.interfaces';
-import {DropdownComponent} from './dropdown.component';
-import {SoapyService, ServicePlaylistData} from './soapy.service';
+import {SoapyService, ServiceAppData} from './soapy.service';
 import * as util from './soapy.utils';
 
 
 @Component({
   providers: [SoapyService],
-  directives: [DropdownComponent],
   selector: 'playback-card',
-  styles: ['.card'],
   templateUrl: '/app/playback.component.html',
 })
 export class PlaybackComponent implements OnInit {
-  public playlists: Playlist[];
-  public selectedPlaylist: Playlist;
+  public playlists: Playlist[] = null;
+  public selectedPlaylist: Playlist = null;
 
   constructor(private _soapyService: SoapyService,
               private _changeDetector: ChangeDetectorRef) {}
 
   public ngOnInit() {
     this._soapyService.getPlaylists()
-    .subscribe((data: ServicePlaylistData) => {
+    .subscribe((data: ServiceAppData) => {
       this.playlists = data.playlists;
-      this.selectedPlaylist = util.findByProperty(
-          this.playlists, 'id', data.selectedPlaylist);
+
+      if (this.playlists) {
+        this.selectedPlaylist = util.findByProperty(
+            this.playlists, 'id', data.selectedPlaylist);
+      }
 
       // Manually detect and propagate changes because:
       // http://stackoverflow.com/a/35106069/3333841

4c8b7eae585d2d46ee9d769d1e4b1f0e0f1fb5f8
Drew Gottlieb <gottlieb.drew@gmail.com>
2016-04-29 19:19:27 -0400

    Web: About card expands to show additional information.

--- Web/typescript/about.card.ts
+++ Web/typescript/about.card.ts
@@ -1,5 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, ElementRef} from 'angular2/core';
 import {StaticData} from './StaticData';
+
+declare var jQuery: JQueryStatic;
 
 
 @Component({
@@ -7,5 +9,30 @@ import {StaticData} from './StaticData';
   template: StaticData.templates.AboutCard,
 })
 export class AboutCardComponent {
+  private $el: JQuery;
+
+  constructor(private el: ElementRef) {
+    this.$el = jQuery(this.el.nativeElement);
+    (<any>window).J = this.$el; // TODO TMP
+  }
+
+  public get $more(): JQuery {
+    return this.$el.find('.more');
+  }
+
+  public get $expandBtn(): JQuery {
+    return this.$el.find('.expand-btn');
+  }
+
+  public expand() {
+    this.$expandBtn.hide();
+    this.$el.css('height', this.$el.outerHeight() + this.$more.outerHeight());
+
+    //this.$el.animate({
+      //height: this.$el.outerHeight() + this.$more.outerHeight(),
+    //});
+
+    return false;
+  }
 }
 

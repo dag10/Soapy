ff025dd0a75bfface424becfed6dd5a613d4a516
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-01-10 19:12:19 -0500

    Web: Added search box to RFID card for pairing with user.

--- Web/typescript/rfid.card.ts
+++ Web/typescript/rfid.card.ts
@@ -28,6 +28,7 @@ export class RfidCardComponent implements AfterViewChecked {
 
   private _id: string;
   private _selectingUser: boolean = false;
+  private _filter: string = null;
 
   constructor(private _el: ElementRef,
               private _changeDetector: ChangeDetectorRef) {
@@ -39,8 +40,16 @@ export class RfidCardComponent implements AfterViewChecked {
     (<any>window).componentHandler.upgradeElements(this._el.nativeElement);
   }
 
-  public get usernameId(): string {
-    return this._id + '-username';
+  public get chipFilterId(): string {
+    return this._id + '-chip-filter';
+  }
+
+  public get filterInputContainer(): HTMLElement {
+    return this._el.nativeElement.querySelector('.mdl-textfield');
+  }
+
+  public get filterInput(): HTMLInputElement {
+    return this._el.nativeElement.querySelector('input');
   }
 
   public get nonSuggestedUsers(): User[] {
@@ -57,7 +66,36 @@ export class RfidCardComponent implements AfterViewChecked {
       });
     }
 
+    if (this._filter !== null) {
+      ret = ret.filter((user: User) => {
+        var filter = this._filter.toLowerCase();
+        var ldap = user.ldap.toLowerCase();
+        var fullName = (user.firstName + ' ' + user.lastName).toLowerCase();
+
+        return ldap.indexOf(filter) >= 0 || fullName.indexOf(filter) >= 0;
+      });
+    }
+
     return ret;
+  }
+
+  public get filterText(): string {
+    return this._filter || '';
+  }
+
+  public setFilter(filter: string) {
+    if (filter && filter.length > 0) {
+      this._filter = filter;
+    } else {
+      this._filter = null;
+
+      // Needed to get the Material placeholder element to be visible.
+      this.filterInputContainer.classList.remove('is-dirty');
+    }
+  }
+
+  public clearFilter() {
+    this.setFilter(null);
   }
 
   public pairWithUser(ldap: string) {
@@ -66,6 +104,24 @@ export class RfidCardComponent implements AfterViewChecked {
 
   public expandUsersList() {
     this._selectingUser = true;
+
+    // Delay to allow template to re-render
+    setTimeout(() => {
+      this.filterInput.focus();
+    }, 50);
+  }
+
+  public collapseUsersList() {
+    this._selectingUser = false;
+    this.clearFilter();
+  }
+
+  public filterKeyDown(event: any) {
+    if (event.keyCode === 27 /* escape key */) {
+      this.collapseUsersList();
+    } else if (event.keyCode === 13 /* enter key */) {
+      this.pairWithUser(this.usersToShow[0].ldap);
+    }
   }
 }
 
